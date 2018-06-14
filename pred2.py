dd7cb785f14fb795da03a3d229b0f6f3974ee650
ATR69 <akshaytrajesh@gmail.com>
2018-06-14 17:12:05 +0530

    minor

---
+++ pred2.py
@@ -0,0 +1,70 @@
+import numpy as np
+from keras.models import load_model
+import pickle
+import sys
+import heapq
+
+np.random.seed(47)
+
+
+model = load_model('keras_model1.h5')
+history = pickle.load(open("history1.p", "rb"))
+
+sequence_len = 30
+
+path = "new_cup.txt"
+rawtxt = open(path).read().lower()
+
+chars = sorted(list(set(rawtxt)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+int_to_char = dict((i, c) for i, c in enumerate(chars))
+
+def prepare_input(text):
+
+    x = np.zeros((1, sequence_len, len(char_to_int)))
+    for t, char in enumerate(text):
+        x[0, t, char_to_int[char]] = 1.        
+    return x
+	
+
+def sample(preds, top_n=3):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds)
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    
+    return heapq.nlargest(top_n, range(len(preds)), preds.take)
+	
+	
+def predict_completion(text):
+    original_text = text
+    generated = text
+    completion = ''
+    while True:
+        x = prepare_input(text)
+        preds = model.predict(x, verbose=0)[0]
+        next_index = sample(preds, top_n=1)[0]
+        next_char = int_to_char[next_index]
+        text = text[1:] + next_char
+        completion += next_char
+        
+        if len(original_text + completion) + 2 > len(original_text) and (next_char == ' ' or next_char == '\n'):
+            return completion
+			
+def predict_completions(text, n=3):
+    x = prepare_input(text)
+    preds = model.predict(x, verbose=0)[0]
+    next_indices = sample(preds, n)
+    r=[int_to_char[idx] + predict_completion(text[1:] + int_to_char[idx]) for idx in next_indices]
+    return [int_to_char[idx] + predict_completion(text[1:] + int_to_char[idx]) for idx in next_indices]
+	
+q=sys.argv[1:]
+q=" ".join(q)
+#q="1 ch"
+seq = q.lower()
+res = predict_completions(seq, 3)
+ret=[]
+for i in res:
+    k=q+i
+    ret.append(k)
+print ret
