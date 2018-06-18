874e0955cf569649b6dabf9a39eda59307a52a9d
ATR69 <akshaytrajesh@gmail.com>
2018-06-18 12:21:13 +0530

    minor

--- predict3.py
+++ predict3.py
@@ -66,7 +66,7 @@ callbacks_list = [checkpoint]
 for epoch in range(1):
 
 	x, y = get_sequence(rawtxt, chars, sequence_len)
-	history = model.fit(x, y, epochs = 1, validation_split = 0.1,  batch_size = 5000, callbacks = callbacks_list, shuffle=True).history
+	history = model.fit(x, y, epochs = 1, validation_split = 0.1,  batch_size = 10000, callbacks = callbacks_list, shuffle=True).history
 	#model.fit(x, y, epochs = 1, validation_split = 0.05,  batch_size = 20, callbacks = callbacks_list)
 
 model.save('keras_model1.h5')
