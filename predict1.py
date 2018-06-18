faefad6464c65e6237ada723bd3fc534083866a4
ATR69 <akshaytrajesh@gmail.com>
2018-06-18 15:13:29 +0530

    minor

--- predict1.py
+++ predict1.py
@@ -43,7 +43,7 @@ def get_sequence(datas):
 			#print datax, '\n'
 			datay.append(data[i])
 			#print datay
-	print type(datax)
+	print datax[3]
 	x = np_utils.to_categorical(datax)
 
 	y = np_utils.to_categorical(datay)
