4b6b3337fc3fbb18637b689d6d7ee202feb1067a
ATR69 <akshaytrajesh@gmail.com>
2018-06-18 17:50:14 +0530

    minor

--- predict5.py
+++ predict5.py
@@ -22,7 +22,7 @@ for i in datas:
 
 
 print len(words)
-print len(list(set(words)))
+print list(set(words))
 word_to_int = dict((c, i) for i, c in enumerate(vocab))
 int_to_word = dict((i, c) for i, c in enumerate(vocab))
 
