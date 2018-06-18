ce52e6beb8c384762fa659ddf642d5aadebb340e
ATR69 <akshaytrajesh@gmail.com>
2018-06-18 17:40:20 +0530

    minor

--- predict5.py
+++ predict5.py
@@ -20,7 +20,7 @@ datas = rawtxt.split("\n")
 for i in datas:
 	words.append(i.split(' '))
 
-vocab = sorted(list(set(words)))
+vocab = sorted(list(set(words[i] for i in words)))
 word_to_int = dict((c, i) for i, c in enumerate(vocab))
 int_to_word = dict((i, c) for i, c in enumerate(vocab))
 
