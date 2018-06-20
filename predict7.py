39697f019bc87ca1ab0fa47417a772ecc5d4d0ea
ATR69 <akshaytrajesh@gmail.com>
2018-06-20 10:54:45 +0530

    minor

--- predict7.py
+++ predict7.py
@@ -34,7 +34,7 @@ def get_sequence(rawtxt, chars, sequence_len):
 		
 		if data != "\n":
 			
-			for i in range (0, len(data), 1):
+			for i in range (1, len(data), 1):
 
 				seq_in = data[:i]
 				#print seq_in
