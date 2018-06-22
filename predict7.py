46b43729fa1708ef85b242a1352dc09322a835ab
ATR69 <akshaytrajesh@gmail.com>
2018-06-22 17:02:31 +0530

    minor

--- predict7.py
+++ predict7.py
@@ -47,11 +47,12 @@ def get_sequence(rawtxt, chars, sequence_len):
 			datax.append([char_to_int[char] for char in seq_in])
 			datay.append([char_to_int[seq_out]])
 
-	n_patterns = len(datax)	
-	#print datax[4][5]
-	print ("Total Pattern : ", n_patterns)
+	n_patterns = len(datax)
 
-	# x = tf.one_hot(datax[0], sequence_len)
+	#print datax[4][5]
+	#print ("Total Pattern : ", n_patterns)
+
+	# x = tf.one_hot(datax[40], sequence_len)
 	# y = tf.one_hot(datay, 1)
 
 	
@@ -70,21 +71,23 @@ def get_sequence(rawtxt, chars, sequence_len):
 
 		y[i, w] = 1
 
-	print "Done"
+	#print "Done"
 
 	return x, y
 
 x, y = get_sequence(rawtxt, chars, sequence_len)
 print (x.shape, '\n', y.shape)
+
+
 model = Sequential()
 model.add(LSTM(256, input_shape=(sequence_len, vocab)))
 #model.add(Bidirectional(LSTM(256, return_sequences = True, activation = 'relu'), input_shape = (sequence_len, vocab)))
-model.add(Dropout(0.4))
+#model.add(Dropout(0.4))
 model.add(Dense(vocab, activation = 'softmax'))
 model.summary()
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-#model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+#optimizer = RMSprop(lr=0.01)
+#model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
 filepath = "wt-imp1.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor = 'loss', verbose = 1, save_best_only = True, mode = 'min')
@@ -93,7 +96,7 @@ callbacks_list = [checkpoint]
 for epoch in range(2):
 
 	x, y = get_sequence(rawtxt, chars, sequence_len)
-	history = model.fit(x, y, epochs = 1, validation_split = 0.1,  batch_size = 1500, callbacks = callbacks_list, shuffle=True).history
+	history = model.fit(x, y, epochs = 1, validation_split = 0.1,  batch_size = 200, callbacks = callbacks_list, shuffle=True).history
 	#model.fit(x, y, epochs = 1, validation_split = 0.05,  batch_size = 20, callbacks = callbacks_list)
 
 model.save('keras_model1.h5')
