f2c754cd0e9ad16cdd98e30b02091beaf5e170f7
ATR69 <akshaytrajesh@gmail.com>
2018-06-28 13:32:59 +0530

    minor

--- predict7.py
+++ predict7.py
@@ -95,7 +95,7 @@ callbacks_list = [checkpoint]
 
 x, y, n = get_sequence(rawtxt, chars, sequence_len)
 
-history = model.fit(x, y, epochs = 2, validation_split = 0.1,  batch_size = 15000, callbacks = callbacks_list, shuffle=True).history
+history = model.fit(x, y, epochs = 2, validation_split = 0.1,  batch_size = 200, callbacks = callbacks_list, shuffle=True).history
 
 # for epoch in range(2):
 
