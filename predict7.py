e8222592903b62c731fa4606ff0b9cd9ca92b5ab
ATR69 <akshaytrajesh@gmail.com>
2018-06-29 10:59:01 +0530

    minor

--- predict7.py
+++ predict7.py
@@ -87,7 +87,7 @@ model.add(Dense(vocab, activation = 'softmax'))
 model.summary()
 # optimizer = RMSprop(lr=0.01)
 # model.compile(loss = 'categorical_crossentropy', optimizer = optimizer, metrics = ['accuracy'])
-model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+model.compile(loss = 'categorical_crossentropy', optimizer = 'adagrad', metrics = ['accuracy'])
 
 filepath = "wt-imp2.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor = 'loss', verbose = 1, save_best_only = True, mode = 'min')
