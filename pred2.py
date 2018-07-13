221b8038d9bfe222965926892432ee55383c471e
Akshay <akshaytrajesh@gmail.com>
2018-07-13 13:38:16 +0530

    commit

--- pred2.py
+++ pred2.py
@@ -64,13 +64,14 @@ def predict_completions(text, n=3):
     r=[int_to_char[idx] + predict_completion(text[1:] + int_to_char[idx]) for idx in next_indices]
     return [int_to_char[idx] + predict_completion(text[1:] + int_to_char[idx]) for idx in next_indices]
 	
-# quotes = [
+quotes = [
     "It is not a lack of love, but a lack of friendship that makes unhappy marriages.",
     "That which does not kill us makes us stronger.",
     "I'm not upset that you lied to me, I'm upset that from now on I can't believe you.",
     "And those who were seen dancing were thought to be insane by those who could not hear the music.",
     "It is hard enough to remember my opinions, without also remembering my reasons for them!"
-]
+    ]
+
 
 for q in quotes:
     seq = q[:40].lower()
